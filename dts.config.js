fb423bcc812d2feb4936758b21430b9549363d92
lumen-limitless <lumenlimitless@gmail.com>
2023-04-26 16:25:57 -0400

    Initial

---
+++ dts.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('dts-cli').DtsConfig}
+ */
+module.exports = {
+  // This function will run for each entry/format/env combination
+  rollup: (config, options) => {
+    options.target = 'node'
+    return config // always return a config.
+  },
+}
